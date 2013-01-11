830928b9549faddafd6692c73502dade58b06002
Rishabh Rao <rishabhsrao@yahoo.com>
2013-01-11 13:15:34 +0530

    Caching document is not required as it's ready cached in the window object.

--- app/scripts/jquery.tooltip.js
+++ app/scripts/jquery.tooltip.js
@@ -1,8 +1,8 @@
-(function($, window, document, undefined) {
+(function($, window, undefined) {
    "use strict";
 
    $.fn.tooltip = function() {
       console.log("Tooltip initialized.");
    };
 
-})(jQuery, window, document);
+})(jQuery, window);
