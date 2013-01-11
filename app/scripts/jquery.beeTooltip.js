43ec7b4b6834576ab687f7d8941e9a7e4aebb245
Rishabh Rao <rishabhsrao@yahoo.com>
2013-01-11 13:18:10 +0530

    Renamed to beeTooltip to avoid namespace clash.

---
+++ app/scripts/jquery.beeTooltip.js
@@ -0,0 +1,8 @@
+(function($, window, undefined) {
+   "use strict";
+
+   $.fn.beeTooltip = function() {
+      console.log("beeTooltip initialized.");
+   };
+
+})(jQuery, window);
--- app/scripts/jquery.tooltip.js
+++
@@ -1,8 +0,0 @@
-(function($, window, undefined) {
-   "use strict";
-
-   $.fn.tooltip = function() {
-      console.log("Tooltip initialized.");
-   };
-
-})(jQuery, window);
