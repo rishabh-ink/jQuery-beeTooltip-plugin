927eeb4baf484ebb8da3cdb0fea38e81cc551c92
Rishabh Rao <rishabhsrao@yahoo.com>
2013-01-11 18:25:15 +0530

    Revert "Added plugin options."
    This reverts commit 47698f38b4b9a43209bbd827c1f166cf36f280e6.

--- app/scripts/jquery.beeTooltip.js
+++ app/scripts/jquery.beeTooltip.js
@@ -1,15 +1,8 @@
 (function($, window, undefined) {
    "use strict";
 
-   $.fn.beeTooltip = function(userOptions) {
-      console.log("$.fn.beeTooltip initialized.");
-      $.fn.beeTooltip.defaults = {
-         speed: 200,
-         delay: 300
-      };
-
-      console.log("Merging defaults with options.", $.fn.beeTooltip.defaults, options);
-      var options = $.extend($.fn.beeTooltip.defaults, userOptions);
+   $.fn.beeTooltip = function() {
+      console.log("beeTooltip initialized.");
    };
 
 })(jQuery, window);
