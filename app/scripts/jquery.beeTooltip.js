ed049f4cf8ff469a7619fa602026b38e0f870221
Rishabh Rao <rishabhsrao@yahoo.com>
2013-01-11 18:48:02 +0530

    Fixed options bug.

--- app/scripts/jquery.beeTooltip.js
+++ app/scripts/jquery.beeTooltip.js
@@ -14,10 +14,23 @@
          var self = this;
 
          self.options = $.extend({}, $.fn.beeTooltip.options, userOptions);
+
+         console.log("BeeTooltip.initialize()", {
+            userOptions: userOptions,
+            self: self,
+            "$.fn.beeTooltip.options": $.fn.beeTooltip.options
+         });
+
+         console.log("$.fn.beeTooltip", {
+            "$.fn.beeTooltip": $.fn.beeTooltip
+         });
       }
    };
 
    $.fn.beeTooltip = function(userOptions) {
+      console.log("$.fn.beeTooltip()", {
+         "this": this
+      });
 
       return this.each(function() {
          var beeTooltip = Object.create(BeeTooltip);
@@ -25,10 +38,10 @@
          beeTooltip.initialize(userOptions);
          // Do your stuff here.
       });
+   };
 
-      $.fn.beeTooltip.options = {
-         effect: "slide"
-      };
+   $.fn.beeTooltip.options = {
+      effect: "slide"
    };
 
 })(jQuery, window, document);
