68e850e463f934706379d8409ac1dc39c91c538e
Rishabh Rao <rishabhsrao@yahoo.com>
2013-01-16 13:16:05 +0530

    Migrated to Github Pages Midnight Theme.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,18 @@
 
    $(document).ready(function() {
       $("[title]").beeTooltip();
+
+      $("#beeTooltip-show").on("click", function(event) {
+         $("#tooltip-trigger").beeTooltip("show");
+
+         event.preventDefault();
+      });
+
+      $("#beeTooltip-hide").on("click", function(event) {
+         $("#tooltip-trigger").beeTooltip("hide");
+
+         event.preventDefault();
+      });
    });
 
 })(jQuery, window, document);
